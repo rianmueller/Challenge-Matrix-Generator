cd5709ecb9ae2a1b4bfdd7c3aea7b29768fe1d03
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-08-07 18:12:22 -1000

    returns an array with right number of rows and columns

--- matrix-generator.js
+++ matrix-generator.js
@@ -1,5 +1,17 @@
 function matrixGenerator(rows, columns) {
   // do work here
+
+  let matrix = [];
+  for (let i = 0; i < rows; i++) {
+    let row = [];
+    for (let j = 0; j < columns; j++) {
+      row.push(Math.floor(Math.random() * 100 + 1));
+    }
+    matrix.push(row);
+  }
+
+  // console.log(matrix);
+  return matrix;
 }
 
 module.exports = matrixGenerator;
