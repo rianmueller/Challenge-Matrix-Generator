c6c349371d77b764b020e176c0947607193204ab
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-08-07 18:23:29 -1000

    returns array with sorted array and sorted nested arrays

--- matrix-generator.js
+++ matrix-generator.js
@@ -1,19 +1,18 @@
 function matrixGenerator(rows, columns) {
-  // do work here
-
   let matrix = [];
   for (let i = 0; i < rows; i++) {
     let row = [];
     for (let j = 0; j < columns; j++) {
       row.push(Math.floor(Math.random() * 100 + 1));
-      row.sort(function(a, b) {
-        return a - b;
-      });
     }
+    row.sort(function(a, b) {
+      return a - b;
+    });
     matrix.push(row);
   }
-
-  // console.log(matrix);
+  matrix.sort(function(a, b) {
+    return a[0] - b[0];
+  });
   return matrix;
 }
 
